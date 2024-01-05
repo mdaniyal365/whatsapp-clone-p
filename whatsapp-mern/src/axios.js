3c153c21e459d66cfd01cf6817f3715295739238
daniyal <mohddaniyal365@gmail.com>
2024-01-05 23:52:12 +0530

    axios

--- whatsapp-mern/src/axios.js
+++ whatsapp-mern/src/axios.js
@@ -5,3 +5,12 @@ const instance = axios.create({
 
 })
 export default instance;
+
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'http://localhost:9001',
+
+})
+export default instance;
+
